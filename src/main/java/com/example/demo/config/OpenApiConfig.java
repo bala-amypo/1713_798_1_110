72c371a37eaa7bcaf23e7ae0030763e157adeb60
bala-amypo <bala@amypo.in>
2025-12-26 13:08:22 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,13 +1,9 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.List;
 
 @Configuration
@@ -15,23 +11,10 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-
-        SecurityScheme bearerScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-
-        SecurityRequirement securityRequirement =
-                new SecurityRequirement().addList("BearerAuth");
-
         return new OpenAPI()
-                .info(new Info()
-                        .title("Digital Complaint Prioritization Engine")
-                        .version("1.0"))
+                // You need to change the port as per your server
                 .servers(List.of(
                         new Server().url("https://9238.pro604cr.amypo.ai/")
-                ))
-                .addSecurityItem(securityRequirement)
-                .schemaRequirement("BearerAuth", bearerScheme);
-    }
+                ));
+        }
 }
